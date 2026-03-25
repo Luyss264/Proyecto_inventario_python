13be03b34075d22365659c2e02d000025450dad0
Luyss264 <joseacevedovargas@gmail.com>
2026-03-24 20:49:25 -0500

    se agregaron nuevas funciones

--- funciones.py
+++ funciones.py
@@ -1,5 +1,31 @@
+import os
+
+
+def menu():
+    
+    os.system('clear')
+        
+    menuPrincipal = """
+    ----------BIENVENIDO AL SISTEMA-----------
+
+    Por favor elija una opción:
+
+    1. -----> Agregar producto.
+    2. -----> Mostrar inventario.
+    3. -----> Calcular estadísticas.
+    4. -----> Buscar.
+    5. -----> Actualizar.
+    6. -----> Eliminar.
+    7. -----> Salir.
+    """
+    
+    print(menuPrincipal)
+
 #se define la función para agregar producto
 def agregar_producto(lista_inventario, productos):
+    
+    os.system('clear')
+    
     print("\n--Agrega un producto--\n")
         
     # Validación del nombre: que no sea solo números
@@ -41,8 +67,15 @@ def agregar_producto(lista_inventario, productos):
     # Cálculo y muestra del resumen del producto
     costo_total = precio_producto * cantidad_producto
     print(f"\nproducto: {nombre_producto} | precio: ${precio_producto} | cantidad: {cantidad_producto} | costo total: ${costo_total}")
+    
+    input("\n---Presione cualquier tecla para continuar---")
+    
+    
+
 
 def mostrar_inventario(lista_inventario):
+    
+    os.system('clear')
     # Verifica si hay productos y los recorre para imprimir sus datos
     if len(lista_inventario) == 0:
         print("\nAún no hay productos en el inventario")
@@ -50,8 +83,13 @@ def mostrar_inventario(lista_inventario):
         for item in lista_inventario:
             for nombre, datos in item.items():
                 print(f"Producto:{nombre} | precio:{datos['precio']} | cantidad:{datos['cantidad']}")
+
+    input("\n---Presione cualquier tecla para continuar---")
                 
 def mostrar_estadisticas(lista_inventario):
+    
+    os.system('clear')
+    
     total_cantidad_por_precio = 0
     total_unidades_fisicas = 0
     
@@ -77,3 +115,77 @@ def mostrar_estadisticas(lista_inventario):
         
     else:
         print("OPCIÓN NO VALIDA")
+    
+    
+    input("\n---Presione cualquier tecla para continuar---")
+        
+        
+def searcher_product(lista_inventario):
+            
+    os.system('clear')
+    
+    search_input = input("\nIngrese el nombre del producto que desea consultar: ")
+    
+    for item in lista_inventario:
+        for name, datos in item.items():
+            
+            if name == search_input.replace(" ","").lower():
+                
+                print("\n----Producto encontrado----")
+                print(f"\nNnombre del producto es {name}")
+                print(f"el precio de {name} es: ${datos['precio']}")
+                print(f"y la cantidad es de: {datos['cantidad']}")
+            
+            else:
+                print("---El producto no existe")
+    
+    input("\n---Presione cualquier tecla para continuar---")
+    
+
+def data_changer(lista_inventario):
+            
+            os.system('clear')
+    
+            search_input = input("\nIngrese el nombre del producto que desea consultar: ")
+
+            for item in lista_inventario:
+                for name, datos in item.items():
+                    
+                    if name == search_input.replace(" ","").lower():
+                        
+                        peticion = input("Desea actualizar datos? Si/No: ").lower()
+                        
+                        if peticion == "si":
+                            datos['precio'] = input("Ingrese el nuevo precio: ")
+                            datos['cantidad'] = input("Ingrese la nueva cantidad: ")
+                        
+                        elif peticion == "no":
+                            break
+                            
+                        else:
+                            print("Ingrese un dato valido por favor")
+                            continue
+                    
+                    else:
+                        print("---El producto no existe")
+
+            input("\n---Presione cualquier tecla para continuar---")
+            
+def delete_product(lista_inventario):
+            
+            os.system('clear')
+    
+            search_input = input("\nIngrese el nombre del producto que desea consultar: ")
+
+            for item in lista_inventario:
+   
+                nombre_en_lista = list(item.keys())[0]
+            
+            
+                if nombre_en_lista.lower() == search_input:
+                    lista_inventario.remove(item)
+                    print(f"\n--- Producto '{nombre_en_lista}' eliminado ---")
+
+
+            input("\n---Presione cualquier tecla para continuar---")
+            
--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 from funciones import * # Importa las funciones del archivo externo
 
-# Definición del menú principal del sistema
-menuPrincipal = """
-----------BIENVENIDO AL SISTEMA-----------
-
-Por favor elija una opción:
-
-1. -----> Agregar producto.
-2. -----> Mostrar inventario.
-3. -----> calcular estadísticas.
-4. -----> Salir.
-"""
-
-opcion = ""
+option = ""
 
 # Estructuras para almacenar los datos
 lista_inventario = []
 productos = {}
 
 # Bucle principal para mantener el programa en ejecución
-while opcion != "4":
+while option != "7":
     
-    print(menuPrincipal)
-    opcion = input("Ingrese el número de la opción que desea: ")
+    menu()
+    
+    option = input("Ingrese el número de la opción que desea: ")
     
     # Llama a la función para registrar productos
-    if opcion == "1":
+    if option == "1":
         agregar_producto(lista_inventario, productos)
     
     # Llama a la función para ver la lista completa
-    elif opcion == "2":
+    elif option == "2":
         mostrar_inventario(lista_inventario)
     
     # Llama a la función para ver totales y cálculos
-    elif opcion == "3":
+    elif option == "3":
         mostrar_estadisticas(lista_inventario)
+        
+    elif option == "4":
+        searcher_product(lista_inventario)
+        
+    elif option == "5":
+        
+        data_changer(lista_inventario)
+        
+    elif option == "6":
+        
+        delete_product(lista_inventario)
     
     # Finaliza la ejecución
-    elif opcion == "4":
+    elif option == "7":
         print("Adiós")
         
     # Manejo de entradas incorrectas
