dd2de990cbd9b849431f7facb19fa3cda51ff7ee
Luyss264 <joseacevedovargas@gmail.com>
2026-03-26 20:37:34 -0500

    se agrega csv en agregar

--- servicios.py
+++ servicios.py
@@ -1,4 +1,9 @@
+import csv
 import os
+
+DATA_CSV = os.path.join('data', 'data.csv')
+
+
 
 def limpiar_pantalla():
     print("\033[H\033[J", end="")
@@ -34,47 +39,68 @@ def agregar_producto(lista_inventario, productos):
     # Validación del nombre: que no sea solo números
     while True:
         nombre_producto = input("Ingrese el nombre del producto: ").lower()
+
+        for item in lista_inventario:
+             if nombre_producto == item['nombre']:
+                  print('\nEste producto ya existe')
+                  continue
+             
         if not nombre_producto.isdigit():
             break
         else:
-            print("Error: El nombre del producto no puede ser un número.")
+            print("\nError: El nombre del producto no puede ser un número.")
         
     # Validación del precio: que sea un entero positivo
     while True:        
         try:    
-            precio_producto = int(input("Ingrese el precio del producto: "))
+            precio_producto = int(input("\nIngrese el precio del producto: "))
             if precio_producto >= 0: 
                 break
 
         except:
-            print("Error: Ingrese un precio válido.")
+            print("\nError: Ingrese un precio válido.")
 
     # Validación de cantidad: que sea un entero positivo
     while True:
         try:       
-            cantidad_producto = int(input("Ingrese la cantidad del producto: "))
+            cantidad_producto = int(input("\nIngrese la cantidad del producto: "))
             if cantidad_producto >= 0: 
                 break
         except:
-            print("Error: Ingrese una cantidad válida.")
+            print("\nError: Ingrese una cantidad válida.")
 
     # Guardar en el diccionario global de productos
-    productos[nombre_producto] = {
+    productos = {
+        "nombre": nombre_producto,
         "precio": precio_producto,
         "cantidad": cantidad_producto,
     }
     
     # Agregar a la lista de inventario y confirmar
-    lista_inventario.append({nombre_producto: productos[nombre_producto]})
+    lista_inventario.append(productos)
     print("\n--Producto agregado--")
+    
+    guardar_csv(lista_inventario)
 
     # Cálculo y muestra del resumen del producto
     costo_total = precio_producto * cantidad_producto
     print(f"\nproducto: {nombre_producto} | precio: ${precio_producto} | cantidad: {cantidad_producto} | costo total: ${costo_total}")
     
     input("\n---Presione cualquier tecla para continuar---")
+
+    return lista_inventario
     
-    
+def guardar_csv(lista_inventario):
+
+    with open(DATA_CSV, 'w', newline='', encoding='utf-8') as f:
+        campos =['nombre', 'precio', 'cantidad']
+        write = csv.DictWriter(f, fieldnames=campos)
+        write.writeheader()
+        for i in lista_inventario:
+                var = {'nombre': i['nombre'], 'precio': i['precio'], 'cantidad': i['cantidad']}
+                write.writerow(var)
+
+
 
 
 def mostrar_inventario(lista_inventario):
@@ -84,9 +110,8 @@ def mostrar_inventario(lista_inventario):
     if len(lista_inventario) == 0:
         print("\nAún no hay productos en el inventario")
     else:
-        for item in lista_inventario:
-            for nombre, datos in list(item.items()):
-                print(f"Producto:{nombre} | precio:{datos['precio']} | cantidad:{datos['cantidad']}")
+        for i in lista_inventario:
+                print(f"Producto: {i['nombre']} | precio:{i['precio']} | cantidad:{i['cantidad']}")
 
     input("\n---Presione cualquier tecla para continuar---")
                 
@@ -98,25 +123,24 @@ def mostrar_estadisticas(lista_inventario):
     total_unidades_fisicas = 0
     
     # Menú de opciones de análisis
-    menuEstadisticas = "\n---Panel de estadisticas---\n1. Total Inventario\n2. Total productos registrados\n"       
+    menuEstadisticas = "\n---Panel de estadisticas---\n1. Total Inventario\n2. Total productos registrados\n3. Producto con mayor precio"       
     print(menuEstadisticas)
     opcionEstadisticas = input("Ingrese el número de la opción: ")
         
     # Opción 1: Calcula el valor monetario total del inventario
     if opcionEstadisticas == "1":
         for item in lista_inventario:
-            for nombre, datos in list(item.items()):
-                total_cantidad_por_precio = total_cantidad_por_precio + (datos['cantidad'] * datos['precio'])
+                total_cantidad_por_precio = total_cantidad_por_precio + (item['cantidad'] * item['precio'])
         print(f"\nEl total monetario es de: ${total_cantidad_por_precio}")
         
     # Opción 2: Calcula unidades totales y variedad de productos
     elif opcionEstadisticas == "2":
         for item in lista_inventario:
-            for nombre, datos in list(item.items()):
-                total_unidades_fisicas += datos['cantidad']
+                total_unidades_fisicas += item['cantidad']
         print(f"\nTotal de unidades físicas: {total_unidades_fisicas}")
         print(f"Total de tipos de productos: {len(lista_inventario)}")
-        
+
+   
     else:
         print("OPCIÓN NO VALIDA")
     
@@ -132,14 +156,13 @@ def buscar_producto(lista_inventario):
     encontrado = False
     
     for item in lista_inventario:
-        for name, datos in list(item.items()):
-            
-            if name == search_input.replace(" ","").lower():
+
+            if item['nombre'] == search_input.replace(" ","").lower():
                 
                 print("\n----Producto encontrado----")
-                print(f"\nNombre del producto es {name}")
-                print(f"el precio de {name} es: ${datos['precio']}")
-                print(f"y la cantidad es de: {datos['cantidad']}")
+                print(f"\nNombre del producto es {item['nombre']}")
+                print(f"el precio de {item['nombre']} es: ${item['precio']}")
+                print(f"y la cantidad es de: {item['cantidad']}")
                 encontrado = True
                 break
             
@@ -156,9 +179,8 @@ def actualizar_producto(lista_inventario):
     search_input = input("\nIngrese el nombre del producto que desea consultar: ")
 
     for item in lista_inventario:
-        for name, datos in list(item.items()):
             
-            if name == search_input.replace(" ","").lower():
+            if item['nombre'] == search_input.replace(" ","").lower():
                 
                 encontrado = True
                 
@@ -174,11 +196,11 @@ def actualizar_producto(lista_inventario):
                 if peticion == "si":
                         while True:
                             try:
-                                datos['precio'] = float(input("\nIngrese el nuevo precio: "))
+                                item['precio'] = float(input("\nIngrese el nuevo precio: "))
                             except:
                                 print("\nDato invalido")
                                 continue
-                            if datos['precio'] < 0:
+                            if item['precio'] < 0:
                                 print("\nNo puede valer menos que 0")
                                 continue
                             else:
@@ -187,11 +209,11 @@ def actualizar_producto(lista_inventario):
 
                         while True:
                             try:
-                                datos['cantidad'] = int(input("\nIngrese la nueva cantidad: "))
+                                item['cantidad'] = int(input("\nIngrese la nueva cantidad: "))
                             except:
                                 print("\nDato invalido")
                                 continue
-                            if datos['precio'] < 0:
+                            if item['precio'] < 0:
                                 print("\nNo puede haber menos que 0")
                                 continue
                             else:
@@ -217,14 +239,12 @@ def eliminar_producto(lista_inventario):
     search_input = input("\nIngrese el nombre del producto que desea consultar: ").lower()
 
     for item in lista_inventario:
-
-        for nombre in item.keys():    
     
-            if nombre == search_input:
+            if item['nombre'] == search_input:
 
                 lista_inventario.remove(item)
                 encontrado = True
-                print(f"\n--- Producto '{nombre}' eliminado ---")
+                print(f"\n--- Producto '{item['nombre']}' eliminado ---")
                 break
     
     if encontrado == False:
